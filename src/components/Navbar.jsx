6b91afd0abf8cb370cafaf7ee5bec79cf6c3bfdd
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2024-04-08 12:05:07 -0300

    style: improving navbar styling and acessibility

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,37 +4,37 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header>
-      <nav className="bg-gray-800 p-4">
+    <header className="fixed top-0 w-full z-50">
+      <nav className="bg-ni-blue-dark p-4">
         <div className="max-w-full mx-auto flex justify-between items-center">
           <div className="flex items-center">
             <div className="flex-shrink-0 mr-4">
               <img className="h-10" src="/logo-sided.jpg" alt="Logo" />
             </div>
           </div>
-          <div className="hidden md:block ml-auto">
+          <div className="hidden font-nunito md:block ml-auto">
             {/* Opções de navegação */}
             <a
               href="#"
-              className="text-white nav-link px-3 py-2 hover:underline"
+              className="text-ni-white  nav-link px-3 py-2 hover:underline hover:text-ni-blue-light"
             >
-              Home
+              Início
             </a>
             <a
               href="#"
-              className="text-white nav-link px-3 py-2 hover:underline"
+              className="text-ni-white nav-link px-3 py-2 hover:underline hover:text-ni-blue-light"
             >
               Sobre
             </a>
             <a
               href="#"
-              className="text-white nav-link px-3 py-2 hover:underline"
+              className="text-ni-white nav-link px-3 py-2 hover:underline hover:text-ni-blue-light"
             >
               Serviços
             </a>
             <a
               href="#"
-              className="text-white nav-link px-3 py-2 hover:underline"
+              className="text-ni-white nav-link px-3 py-2 hover:underline hover:text-ni-blue-light"
             >
               Contato
             </a>
@@ -55,9 +55,9 @@ const Navbar = () => {
         </div>
         {/* Menu burger para dispositivos móveis */}
         {isOpen && (
-          <div className="md:hidden mt-2">
+          <div className="md:hidden font-nunito mt-2">
             <a href="#" className="block text-center text-white px-3 py-2">
-              Home
+              Início
             </a>
             <a href="#" className="block text-center text-white px-3 py-2">
               Sobre
