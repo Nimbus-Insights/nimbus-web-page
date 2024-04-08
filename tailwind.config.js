b69ccd05cf9114f3a86f1775fb766c1cbe02c797
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2024-04-08 12:04:44 -0300

    style: custom tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ni-white": "#ffffee",
+        "ni-yellow": "#ffbf00",
+        "ni-blue": {
+          dark: "#0b2a49",
+          medium: "#3495c8",
+          light: "#76dfff",
+        },
+      },
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
