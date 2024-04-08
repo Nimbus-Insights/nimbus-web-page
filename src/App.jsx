c300000add977a44355cb71d47fd826d3436d7f9
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2024-04-08 19:47:45 -0300

    style: improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import bgMain from "./assets/bg-main4.svg"; // Importe a imagem SVG
 
 function App() {
   return (
@@ -6,13 +7,13 @@ function App() {
       <Navbar />
       <main>
         <section
-          className="h-screen top-16 bg-cover bg-center flex flex-col justify-center items-center"
-          style={{ backgroundImage: "url('/sua-imagem-de-fundo.jpg')" }}
+          className="h-screen top-8 bg-cover bg-center flex flex-col justify-center items-start"
+          style={{ backgroundImage: `url(${bgMain})` }}
         >
-          <h1 className="text-ni-yellow text-4xl md:text-6xl font-bold text-center mb-4">
+          <h1 className="text-ni-yellow text-4xl md:text-6xl font-roboto text-left mt-8 mb-4 md:px-36 sm:px-10 px-10">
             Nimbus Insights
           </h1>
-          <p className="text-white text-lg md:text-xl text-center">
+          <p className="text-white text-lg md:text-xl font-roboto text-left mt-4 md:px-36 sm:px-10 px-10">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, aut
             ea, corrupti, error natus officiis quo tenetur repellendus.
           </p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,27 +16,27 @@ const Navbar = () => {
             {/* Opções de navegação */}
             <a
               href="#"
-              className="text-ni-white  nav-link px-3 py-2 hover:underline hover:text-ni-blue-light"
+              className="text-ni-blue-dark  nav-link px-5 py-2 hover:underline hover:text-blue-800 bg-ni-white rounded-full"
             >
-              Início
+              Home
             </a>
             <a
               href="#"
-              className="text-ni-white nav-link px-3 py-2 hover:underline hover:text-ni-blue-light"
+              className="text-ni-white nav-link px-5 py-2 hover:underline hover:text-ni-blue-light"
             >
-              Sobre
+              About
             </a>
             <a
               href="#"
-              className="text-ni-white nav-link px-3 py-2 hover:underline hover:text-ni-blue-light"
+              className="text-ni-white nav-link px-5 py-2 hover:underline hover:text-ni-blue-light"
             >
-              Serviços
+              Services
             </a>
             <a
               href="#"
-              className="text-ni-white nav-link px-3 py-2 hover:underline hover:text-ni-blue-light"
+              className="text-ni-white nav-link px-5 py-2 hover:underline hover:text-ni-blue-light"
             >
-              Contato
+              Contact us
             </a>
           </div>
           <div className="md:hidden">
@@ -61,16 +61,16 @@ const Navbar = () => {
             }`}
           >
             <a href="#" className="block text-center text-white px-3 py-2">
-              Início
+              Home
             </a>
             <a href="#" className="block text-center text-white px-3 py-2">
-              Sobre
+              About
             </a>
             <a href="#" className="block text-center text-white px-3 py-2">
-              Serviços
+              Services
             </a>
             <a href="#" className="block text-center text-white px-3 py-2">
-              Contato
+              Contact us
             </a>
           </div>
         )}
