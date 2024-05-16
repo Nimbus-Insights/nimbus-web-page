f2672c0b3f231df5c45b0802225317bc94868ae1
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2024-05-16 15:16:20 -0300

    feat: under construcion msg

--- src/App.jsx
+++ src/App.jsx
@@ -30,20 +30,7 @@ function App() {
             Nimbus Insights
           </h1>
           <p className="text-white text-lg md:text-3xl font-roboto text-left mt-4 md:px-36 sm:px-10 px-10">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, aut
-            ea, corrupti, error natus officiis quo tenetur repellendus.
-          </p>
-        </section>
-        <section
-          className="h-screen top-8 bg-cover bg-center flex flex-col justify-center items-start"
-          style={{ backgroundImage: `url(${bgMain})` }}
-        >
-          <h1 className="text-ni-yellow text-4xl md:text-9xl font-roboto text-left mt-8 mb-4 md:px-36 sm:px-10 px-10">
-            Nimbus Insights
-          </h1>
-          <p className="text-white text-lg md:text-3xl font-roboto text-left mt-4 md:px-36 sm:px-10 px-10">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, aut
-            ea, corrupti, error natus officiis quo tenetur repellendus.
+            Website under construction.
           </p>
         </section>
       </main>
