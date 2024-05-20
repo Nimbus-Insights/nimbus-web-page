935f25b88a2179c220b425c13b2f2005a376fc02
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2024-05-20 12:54:13 -0300

    feat: update tempData

--- src/data/tempData.js
+++ src/data/tempData.js
@@ -1,12 +1,14 @@
 const tempData = [
   {
     name: "Aster Mineral Index Threshold",
-    description: "App for to aquire mineral indexes from Aster images",
+    description:
+      "In this application, we implemented an adjustable threshold for the selected mineral indices, allowing the user to create masks by determining the threshold values for visualizing the mineral indices. This application can also be adapted for indices calculated from other images such as Landsat, Sentinel, WorldView, among others.",
     url: "https://ee-nimbusinsights.projects.earthengine.app/view/astermineralindexthreshold",
   },
   {
     name: "Aster Mineral Index References",
-    description: "App for to aquire mineral indexes from Aster images",
+    description:
+      "This application is designed for geological mapping and mineral exploration. In the application, the entire preprocessing of the ASTER collection has been automated. Additionally, various mineral indices available in academic literature have been calculated and plotted. The application can be adapted for any area of the globe covered by the ASTER collection. The bibliographic references for each calculated index are available in the application's information card.",
     url: "https://ee-nimbusinsights.projects.earthengine.app/view/astermineralindexreferences",
   },
 ];
