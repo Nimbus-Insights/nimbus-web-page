c18bb987a0b3cf35276bad60726fffa9aa5e6398
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2024-05-24 21:18:33 -0300

    style: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,61 +1,90 @@
 import instagramLogo from "../assets/instagram.svg";
 import linkedinLogo from "../assets/linkedin.svg";
 import youtubeLogo from "../assets/youtube.svg";
+import logoSidedTransparency from "../assets/nimbus-sided-logo-transparency.png";
 
 function Footer() {
   return (
-    <footer className="h-48 w-full border-t-2 border-ni-yellow flex flex-row justify-evenly items-center ">
-      {/* LOGO START */}
-      <div>logo Nimbus</div>
-      {/* LOGO END */}
-      {/* CONTACTS START */}
-      <div className="flex flex-col justify-evenly items-center">
-        <h6 className="mb-10 -mt-1 text-ni-yellow text-2xl">Contact Us</h6>
-        <ul>
-          <li>
-            <a href="mailto:nimbusinsights@gmail.com?subject=Assunto do email&bcc=murillo@nimbusinsights.cloud, kim@nimbusinsights.cloud">
-              <p className="text-ni-yellow text-xl underline">
-                Send us an e-mail
-              </p>
-            </a>
-          </li>
-        </ul>
+    <footer className="bg-gray-900 text-white">
+      <div className="container mx-auto py-8 px-4 md:px-0">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          {/* LOGO */}
+          <div className="flex justify-center items-center mb-4 md:mb-0">
+            <img
+              src={logoSidedTransparency}
+              alt="Nimbus Logo"
+              className="h-20 md:h-24"
+            />
+          </div>
+          {/* CONTACTS */}
+          <div className="flex flex-col justify-center items-center mb-8 md:mb-0">
+            <h6 className="mb-4 text-ni-white text-xl">Contact Us</h6>
+            <ul className="text-center">
+              <li className="mb-2">
+                <a
+                  href="mailto:nimbusinsights@gmail.com?subject=Assunto do email&bcc=murillo@nimbusinsights.cloud, kim@nimbusinsights.cloud"
+                  className="text-gray-300 hover:text-ni-white transition-colors duration-300 und"
+                >
+                  Send us an e-mail
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/* SOCIAL MEDIA */}
+          <div className="flex justify-center items-center">
+            <ul className="flex flex-row space-x-4">
+              <li>
+                <a
+                  href="https://www.linkedin.com/company/nimbusinsights"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-transform duration-300 hover:scale-110"
+                >
+                  <img
+                    src={linkedinLogo}
+                    alt="Linkedin Logo"
+                    className="h-8 md:h-10"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/nimbusinsights"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-transform duration-300 hover:scale-110"
+                >
+                  <img
+                    src={instagramLogo}
+                    alt="Instagram Logo"
+                    className="h-8 md:h-10"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.youtube.com/channel/nimbusinsights"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-transform duration-300 hover:scale-110"
+                >
+                  <img
+                    src={youtubeLogo}
+                    alt="Youtube Logo"
+                    className="h-8 md:h-10"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="text-center mt-4">
+          <p className="text-xs sm:text-sm md:text-xs lg:text-sm xl:text-xs">
+            &copy; {new Date().getFullYear()} Nimbus Insights. All rights
+            reserved.
+          </p>
+        </div>
       </div>
-      {/* CONTACTS END */}
-      {/* SOCIAL MEDIA START */}
-      <div className="flex flex-col justify-evenly items-center">
-        <h6 className="mb-10 text-ni-yellow text-2xl">Social Medias</h6>
-        <ul className="flex flex-row">
-          <li className="mx-3.5">
-            <a href="https://www.linkedin/in/nimbusinsights" target="_blank">
-              <img
-                src={linkedinLogo}
-                alt="Linkedin Logo, refers to linkedin page"
-                className="transition-transform duration-500 ease-in-out transform hover:scale-125"
-              />
-            </a>
-          </li>
-          <li className="mx-3.5">
-            <a href="https://www.instagram.com/nimbusinsights" target="_blank">
-              <img
-                src={instagramLogo}
-                alt="Instagram Logo, refers to instagram page"
-                className="transition-transform duration-500 ease-in-out transform hover:scale-125"
-              />
-            </a>
-          </li>
-          <li className="mx-3.5">
-            <a href="https://www.youtube.com/@nimbusinsights" target="_blank">
-              <img
-                src={youtubeLogo}
-                alt="Youtube Logo, refers to youtbe page"
-                className="transition-transform duration-500 ease-in-out transform hover:scale-125"
-              />
-            </a>
-          </li>
-        </ul>
-      </div>
-      {/* SOCIAL MEDIA END */}
     </footer>
   );
 }
