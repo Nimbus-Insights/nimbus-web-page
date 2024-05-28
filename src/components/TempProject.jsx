a918d1251925a6733526fe7df7728a45a6d04732
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2024-05-28 18:25:16 -0300

    style: tempProjects card stylized

--- src/components/TempProject.jsx
+++ src/components/TempProject.jsx
@@ -1,7 +1,7 @@
 const TempProject = ({ name, description, url }) => {
   return (
     <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg mb-8">
+      <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 mb-8">
         <div className="px-6 py-4">
           <h5 className="text-ni-yellow text-3xl md:text-4xl font-nunito text-left mt-4 mb-2">
             <a
