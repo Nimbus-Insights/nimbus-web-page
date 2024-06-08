a198e071078af43997162871bc629e2cb9c141a3
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2024-06-08 12:12:11 -0300

    feat: update data

--- src/data/tempData.js
+++ src/data/tempData.js
@@ -23,6 +23,17 @@ const tempData = [
       "This application is designed for geological mapping and mineral exploration. In the application, the entire preprocessing of the ASTER collection has been automated. Additionally, various mineral indices available in academic literature have been calculated and plotted. The application can be adapted for any area of the globe covered by the ASTER collection. The bibliographic references for each calculated index are available in the application's information card.",
     url: "https://ee-nimbusinsights.projects.earthengine.app/view/astermineralindexreferences",
   },
+  {
+    name: "PCA Sentinel-2 Pegmatite Seridó",
+    description:
+      "This application utilizes the split slider functionality to compare an RGB image from the Sentinel satellite with a color composition of the new vectors extracted by Principal Component Analysis (PCA) in the Seridó region. This feature allows users to interactively visualize the differences and correlations between the original data and the PCA results, facilitating the identification of potential areas for pegmatite prospecting.",
+    url: "https://ee-nimbusinsights.projects.earthengine.app/view/pagmatitosseridopca1sentinel",
+  },
+  {
+    name: "MODIS - Monthly Burned Areas - 500m",
+    description: "This application leverages the MODIS (Moderate Resolution Imaging Spectroradiometer) sensor data to provide monthly monitoring of burned areas worldwide at a 500-meter resolution. Users can select the year and visualize the burned areas, color-coded by quarter, enabling detailed analysis and tracking of fire patterns over time.",
+    url: "https://ee-nimbusinsights.projects.earthengine.app/view/modismonthlyburnedareas",
+  },
 ];
 
 export default tempData;
